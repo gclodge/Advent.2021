211117f1506c2649fc59ad58a8be5845b227a292
gclodge <graham.clodge@gmail.com>
2021-12-02 08:54:28 -0500

    Finished Day Two

---
+++ src/Advent.Solutions/Days/Day.02.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+
+namespace Advent.Solutions.Days
+{
+    public class Submarine
+    {
+        public int HorizontalPosition { get; set; } = 0;
+        public int Depth { get; set; } = 0;
+        public int Aim { get; set; } = 0;
+
+        public int Value => Depth * HorizontalPosition;
+
+        public List<string> Commands { get; set; }
+
+        bool UseAim { get; } = false;
+
+        public Submarine(IEnumerable<string> commands, bool useAim = false)
+        {
+            this.Commands = commands.ToList();
+            this.UseAim = useAim;
+        }
+
+        public void ChartCourse()
+        {
+            foreach (var command in this.Commands)
+            {
+                HandleCommand(command);
+            }
+        }
+
+        void HandleCommand(string command)
+        {
+            var cmd = Parse(command);
+            switch (cmd.dir)
+            {
+                case "forward":
+                    HorizontalPosition += cmd.mag;
+                    if (UseAim) { Depth += (cmd.mag * Aim); }
+                    break;
+                case "up":
+                    if (UseAim) { Aim -= cmd.mag; }
+                    else { Depth -= cmd.mag; }
+                    break;
+                case "down":
+                    if (UseAim) { Aim += cmd.mag; }
+                    else { Depth += cmd.mag; }
+                    break;
+                default:
+                    throw new NotImplementedException($"Unknown command: {command}");
+            }
+        }
+
+        static (string dir, int mag) Parse(string command)
+        {
+            var parts = command.Split(' ');
+            return (parts[0].ToLower(), int.Parse(parts[1]));
+        }
+    }
+}
---
+++ tests/Advent.Tests/DailyTests/Day.02.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+using Advent.Solutions.Utility;
+using Advent.Solutions.Days;
+
+namespace Advent.Tests.DailyTests
+{
+    public class Day02 : Interfaces.IDailyTest
+    {
+        public int Number => 2;
+
+        public string Input => TestHelper.GetInputFile(this);
+
+        [Fact]
+        public void Test_KnownCommands()
+        {
+            var cmds = new List<string>()
+            {
+                "forward 5",
+                "down 5",
+                "forward 8",
+                "up 3",
+                "down 8",
+                "forward 2"
+            };
+
+            var sub = new Submarine(cmds);
+            sub.ChartCourse();
+
+            int expectedPos = 15;
+            int expectedDepth = 10;
+            int expectedValue = expectedPos * expectedDepth;
+
+            Assert.Equal(sub.HorizontalPosition, expectedPos);
+            Assert.Equal(sub.Depth, expectedDepth);
+            Assert.Equal(sub.Value, expectedValue);
+
+            var aimSub = new Submarine(cmds, useAim: true);
+            aimSub.ChartCourse();
+
+            expectedPos = 15;
+            expectedDepth = 60;
+            expectedValue = expectedPos * expectedDepth;
+
+            Assert.Equal(aimSub.HorizontalPosition, expectedPos);
+            Assert.Equal(aimSub.Depth, expectedDepth);
+            Assert.Equal(aimSub.Value, expectedValue);
+        }
+
+        [Fact]
+        public void PartOne()
+        {
+            var sub = new Submarine(Input.Parse());
+            sub.ChartCourse();
+
+            int expectedValue = 2091984;
+            Assert.Equal(expectedValue, sub.Value);
+        }
+
+        [Fact]
+        public void PartTwo()
+        {
+            var sub = new Submarine(Input.Parse(), useAim: true);
+            sub.ChartCourse();
+
+            int expectedValue = 2086261056;
+            Assert.Equal(expectedValue, sub.Value);
+        }
+    }
+}
