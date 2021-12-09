f3c0378deba669730afe381b634b00852851af15
gclodge <graham.clodge@gmail.com>
2021-12-09 16:03:22 -0500

    Finished day nine

---
+++ src/Advent.Solutions/Days/Day.09.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Solutions.Days
+{
+    using GridMap = Dictionary<int, Dictionary<int, int>>;
+
+    internal record Basin(int Index, int Size);
+    internal record LowPoint(int X, int Y, int Risk);
+
+    public class VolcanicHeightMap
+    {
+        GridMap HeightMap;
+        GridMap BasinMap;
+
+        const int DefaultBasinGroup = -1;
+        const int NonBasinGroup = -2;
+
+        List<LowPoint> LowPoints;
+        List<Basin> Basins;
+
+        public int LowPointCount => LowPoints.Count();
+        public int LowPointRisk => LowPoints.Sum(x => x.Risk);
+
+        public int BasinCount => Basins.Count();
+        public int LargestBasinProduct => GetLargestBasinProduct();
+
+        public VolcanicHeightMap(IEnumerable<string> inputs)
+        {
+            LowPoints = new List<LowPoint>();
+            Basins = new List<Basin>();
+
+            HeightMap = new GridMap();
+            BasinMap = new GridMap();
+
+            foreach (int y in Enumerable.Range(0, inputs.Count()))
+            {
+                string item = inputs.ElementAt(y);
+                foreach (int x in Enumerable.Range(0, item.Length))
+                {
+                    int val = int.Parse(item[x].ToString());
+                    AddValue(val, x, y);
+                }
+            }
+        }
+
+        public VolcanicHeightMap GetLowPointRisk()
+        {
+            foreach (int y in HeightMap.Keys)
+            {
+                foreach (int x in HeightMap[y].Keys)
+                {
+                    if (IsLowPoint(x, y))
+                    {
+                        LowPoints.Add(new LowPoint(x, y, GetRisk(x, y)));
+                    }
+                }
+            }
+
+            return this;
+        }
+
+        public VolcanicHeightMap GenerateBasinGroups()
+        {
+            for (int i = 0; i < LowPoints.Count; i++)
+            {
+                var basin = GenerateBasin(LowPoints[i], i);
+                Basins.Add(basin);
+            }
+
+            return this;
+        }
+
+        void AddValue(int val, int x, int y)
+        {
+            if (!HeightMap.ContainsKey(y))
+            {
+                HeightMap.Add(y, new Dictionary<int, int>());
+                BasinMap.Add(y, new Dictionary<int, int>());
+            }
+                
+            HeightMap[y].Add(x, val);
+            BasinMap[y].Add(x, val == 9 ? NonBasinGroup : DefaultBasinGroup);
+        }
+
+        bool IsLowPoint(int x, int y)
+        {
+            int currVal = GetValue(x, y);
+            var values = GetNeighbours(x, y)
+                            .Select(n => GetValue(n.X, n.Y));
+
+            return values.All(v => v > currVal);
+        }
+
+        IEnumerable<(int X, int Y)> GetNeighbours(int x, int y)
+        {
+            var neigh = new (int X, int Y)[]
+            {
+                new(x - 1, y),
+                new(x + 1, y),
+                new(x, y - 1),
+                new(x, y + 1)
+            };
+
+            return neigh.Where(n => HeightMap.ContainsKey(n.Y) && HeightMap[n.Y].ContainsKey(n.X));
+        }
+
+        int GetValue(int x, int y)
+        {
+            return HeightMap[y][x];
+        }
+
+        int GetRisk(int x, int y)
+        {
+            return 1 + GetValue(x, y);
+        }
+
+        int GetBasin(int x, int y)
+        {
+            return BasinMap[y][x];
+        }
+
+        void SetBasin(int x, int y, int group)
+        {
+            BasinMap[y][x] = group;
+        }
+
+        Basin GenerateBasin(LowPoint lp, int group)
+        {
+            var stack = new Stack<(int X, int Y)>();
+            stack.Push(new(lp.X, lp.Y));
+
+            while(stack.Count > 0)
+            {
+                var pos = stack.Pop();
+                SetBasin(pos.X, pos.Y, group);
+
+                int currVal = GetValue(pos.X, pos.Y);
+                var neighs = GetNeighbours(pos.X, pos.Y);
+                foreach (var neigh in neighs)
+                {
+                    if (GetBasin(neigh.X, neigh.Y) == DefaultBasinGroup
+                           && GetValue(neigh.X, neigh.Y) > currVal)
+                    {
+                        SetBasin(pos.X, pos.Y, group);
+                        stack.Push(neigh);
+                    }
+                }
+            }
+
+            int count = BasinMap.Sum(y => y.Value.Count(v => v.Value == group));
+            return new Basin(group, count);
+        }
+
+        int GetLargestBasinProduct()
+        {
+            var sizes = Basins.OrderByDescending(b => b.Size).Take(3).Select(b => b.Size);
+            return sizes.Aggregate((total, next) => total * next);
+        }
+    }
+}
---
+++ tests/Advent.Tests/DailyTests/Day.09.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+using Advent.Solutions.Utility;
+using Advent.Solutions.Days;
+
+namespace Advent.Tests.DailyTests
+{
+    public class Day09 : Interfaces.IDailyTest
+    {
+        public int Number => 9;
+
+        public string Input => TestHelper.GetInputFile(this);
+        public string Test => TestHelper.GetTestFile(this);
+
+        [Fact]
+        public void Test_KnownInputs()
+        {
+            var input = new string[]
+            {
+                "2199943210",
+                "3987894921",
+                "9856789892",
+                "8767896789",
+                "9899965678"
+            };
+
+            var map = new VolcanicHeightMap(input)
+                            .GetLowPointRisk()
+                            .GenerateBasinGroups();
+
+            int expectedRisk = 15;
+            int expectedCount = 4;
+
+            Assert.Equal(expectedRisk, map.LowPointRisk);
+            Assert.Equal(expectedCount, map.LowPointCount);
+
+            int expectedBasins = 4;
+            int expectedBasinProduct = 1134;
+
+            Assert.Equal(expectedBasins, map.BasinCount);
+            Assert.Equal(expectedBasinProduct, map.LargestBasinProduct);
+        }
+
+        [Fact]
+        public void PartOne()
+        {
+            var map = new VolcanicHeightMap(Input.Parse())
+                            .GetLowPointRisk();
+
+            int expectedRisk = 537;
+            int expectedCount = 228;
+
+            Assert.Equal(expectedRisk, map.LowPointRisk);
+            Assert.Equal(expectedCount, map.LowPointCount);
+        }
+
+        [Fact]
+        public void PartTwo()
+        {
+            var map = new VolcanicHeightMap(Input.Parse())
+                            .GetLowPointRisk()
+                            .GenerateBasinGroups();
+
+            int expectedBasins = 228;
+            int expectedBasinProduct = 1142757;
+
+            Assert.Equal(expectedBasins, map.BasinCount);
+            Assert.Equal(expectedBasinProduct, map.LargestBasinProduct);
+        }
+    }
+}
