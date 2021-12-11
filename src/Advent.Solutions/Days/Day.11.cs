6cffb5080196b85343fa94a8937dd1b3162dc2d5
gclodge <graham.clodge@gmail.com>
2021-12-11 12:09:53 -0500

    Finished day eleven

---
+++ src/Advent.Solutions/Days/Day.11.cs
@@ -0,0 +1,141 @@
+﻿namespace Advent.Solutions.Days
+{
+    using GridMap = Dictionary<int, Dictionary<int, int>>;
+
+    public class OctopusGrid
+    {
+        const int FlashPointValue = 9;
+
+        GridMap OctoMap;
+
+        public int Flashed { get; private set; } = 0;
+
+        public List<(int Step, int Flashed)> Counter = new();
+
+        public OctopusGrid(IEnumerable<string> inputs)
+        {
+            OctoMap = new();
+
+            foreach (int y in Enumerable.Range(0, inputs.Count()))
+            {
+                string item = inputs.ElementAt(y);
+                foreach (int x in Enumerable.Range(0, item.Length))
+                {
+                    int val = int.Parse(item[x].ToString());
+                    AddValue(val, x, y);
+                }
+            }
+        }
+
+        public (int flashCount, int synchStep) Solve()
+        {
+            int s100 = 0;
+
+            int step = 1;
+            while (!Synchronized())
+            {
+                SimulateStep();
+                
+                if (step == 100)
+                {
+                    s100 = Flashed;
+                }
+
+                step += 1;
+            }
+
+            return (s100, step - 1);
+        }
+
+        void SimulateStep()
+        {
+            var stack = new Stack<(int X, int Y)>();
+
+            foreach (int y in OctoMap.Keys)
+            {
+                foreach (int x in OctoMap[y].Keys)
+                {
+                    int val = IncrementValue(x, y);
+                    if (val > FlashPointValue)
+                    {
+                        SetValue(0, x, y);
+                        Flashed++;
+                        stack.Push(new(x, y));
+                    }
+                }
+            }
+
+            while (stack.Count > 0)
+            {
+                var curr = stack.Pop();
+                foreach (var n in GetNeighbours(curr.X, curr.Y))
+                {
+                    if (OctoMap[n.Y][n.X] != 0)
+                    {
+                        int val = IncrementValue(n.X, n.Y);
+                        if (val > FlashPointValue)
+                        {
+                            SetValue(0, n.X, n.Y);
+                            Flashed++;
+                            stack.Push(n);
+                        }
+                    }
+                }
+            }
+        }
+
+        bool Synchronized()
+        {
+            foreach (int y in OctoMap.Keys)
+            {
+                foreach (int x in OctoMap[y].Keys)
+                {
+                    if (OctoMap[y][x] != 0)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        void AddValue(int val, int x, int y)
+        {
+            if (!OctoMap.ContainsKey(y))
+            {
+                OctoMap.Add(y, new Dictionary<int, int>());
+            }
+            OctoMap[y].Add(x, val);
+        }
+
+        void SetValue(int val, int x, int y)
+        {
+            OctoMap[y][x] = val;
+        }
+
+        int IncrementValue(int x, int y)
+        {
+            if (OctoMap[y][x] >= 0)
+            {
+                OctoMap[y][x] += 1;
+            }
+
+            return OctoMap[y][x];
+        }
+
+        IEnumerable<(int X, int Y)> GetNeighbours(int x, int y)
+        {
+            var neigh = new (int X, int Y)[]
+            {
+                new(x - 1, y),
+                new(x - 1, y + 1),
+                new(x - 1, y - 1),
+                new(x + 1, y),
+                new(x + 1, y + 1),
+                new(x + 1, y - 1),
+                new(x, y - 1),
+                new(x, y + 1)
+            };
+
+            return neigh.Where(n => OctoMap.ContainsKey(n.Y) && OctoMap[n.Y].ContainsKey(n.X));
+        }
+    }
+}
---
+++ tests/Advent.Tests/DailyTests/Day.11.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+using Advent.Solutions.Utility;
+using Advent.Solutions.Days;
+
+namespace Advent.Tests.DailyTests
+{
+    public class Day11 : Interfaces.IDailyTest
+    {
+        public int Number => 11;
+
+        public string Input => TestHelper.GetInputFile(this);
+        public string Test => TestHelper.GetTestFile(this);
+
+        [Fact]
+        public void Test_KnownInputs()
+        {
+            var input = Test.Parse();
+            var octo = new OctopusGrid(input);
+            var res = octo.Solve();
+
+            int expectedSync = 195;
+            int expectedFlashes = 1656;
+            Assert.Equal(expectedFlashes, res.flashCount);
+            Assert.Equal(expectedSync, res.synchStep);
+        }
+
+        [Fact]
+        public void PartOne()
+        {
+            var input = Input.Parse();
+            var octo = new OctopusGrid(input);
+            var res = octo.Solve();
+
+            int expectedFlashes = 1562;
+            Assert.Equal(expectedFlashes, res.flashCount);
+        }
+
+        [Fact]
+        public void PartTwo()
+        {
+            var input = Input.Parse();
+            var octo = new OctopusGrid(input);
+            var res = octo.Solve();
+
+            int expectedSynchStep = 268;
+            Assert.Equal(expectedSynchStep, res.synchStep);
+        }
+    }
+}
