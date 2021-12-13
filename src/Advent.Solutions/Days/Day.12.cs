719fd4af25904d2eb63c15297c63bd1d96c06d6c
gclodge <graham.clodge@gmail.com>
2021-12-13 12:14:01 -0500

    Cleanup

--- src/Advent.Solutions/Days/Day.12.cs
+++ src/Advent.Solutions/Days/Day.12.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Advent.Solutions.Days
+﻿namespace Advent.Solutions.Days
 {
     public class PassagePath
     {
@@ -106,44 +100,5 @@ namespace Advent.Solutions.Days
         {
             return char.IsLower(cave[0]);
         }
-
-        /*
-         * 
-
-        /// <summary>
-        /// Recursively try each cave next to this one, keeping track of the step counts we
-        /// are at when we visit each cave. This allows us to backtrack by forgetting any caves
-        /// we "visited in the future" after returning back from a recursive call
-        /// </summary>
-        /// <param name="cave"></param>
-        /// <param name="steps">Number of steps taken to get here</param>
-        void SearchFrom(string cave, int steps)
-        {
-            // if we've arrived at a small cave, record when we got here
-            if (IsSmallCave(cave))
-                _pastVisits[cave].Add(steps);
-
-            // explore all valid paths outwards from here one at a time
-            foreach (var next in _from[cave])
-            {                
-
-                // we have permission to explore this cave, so recursively call this again
-                SearchFrom(next, steps + 1);
-
-                // having returned from an arbitrarily long path, we have to undo the effects
-                // of the path-taking by forgetting the visited status of any cave that we
-                // visited after this older call of SearchFrom that we're now back in
-                foreach (var k in _pastVisits.Keys)
-                {
-                    // keep only this and prior step counts
-                    _pastVisits[k] = _pastVisits[k]
-                        .Where(s => s <= steps)
-                        .ToList();
-                }
-            }
-        }
-
-         */
-
     }
 }
