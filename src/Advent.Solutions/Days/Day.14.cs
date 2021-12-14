e88036fbe9a88735eb83050620355928ea4cf178
gclodge <graham.clodge@gmail.com>
2021-12-14 10:49:36 -0500

    Finished day fourteen

---
+++ src/Advent.Solutions/Days/Day.14.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Solutions.Days
+{
+    public class Polymerization
+    {
+        public string Template { get; }
+
+        Dictionary<string, string> Swaps = new();
+
+        Dictionary<string, long> Counts = new();
+
+        public Polymerization(IEnumerable<string> input)
+        {
+            Template = input.First();
+
+            foreach (var i in Enumerable.Range(0, Template.Length - 1))
+            {
+                var str = Template.Substring(i, 2);
+                Counts[str] = Counts.GetValueOrDefault(str) + 1;
+            }
+
+            foreach (var line in input.Skip(2))
+            {
+                var arr = line.Split(" -> ");
+                Swaps.Add(arr[0], arr[1]);
+            }
+        }
+
+        public long Solve(int steps = 10)
+        {
+            //< Apply all required steps
+            foreach (int i in Enumerable.Range(0, steps)) Step();
+
+            //< Generate the final histogram count
+            return CalculateResult();
+        }
+
+        void Step()
+        {
+            this.Counts = Counts.SelectMany(GetSubstitutions)
+                                .GroupBy(kvp => kvp.Key)
+                                .ToDictionary(grp => grp.Key, grp => grp.Sum(g => g.Value));
+        }
+
+        Dictionary<string, long> GetSubstitutions(KeyValuePair<string, long> kvp)
+        {
+            return new Dictionary<string, long>()
+            {
+                { kvp.Key[0] + Swaps[kvp.Key], kvp.Value },
+                { Swaps[kvp.Key] + kvp.Key[1], kvp.Value }
+            };
+        }
+
+        long CalculateResult()
+        {
+            var results = Swaps.SelectMany(s => s.Key.ToCharArray())
+                               .Distinct()
+                               .Select(c => Counts.Sum(kvp => kvp.Key[1] == c ? kvp.Value : 0) +
+                                            (c == Counts.First().Key[0] ? Counts.First().Value : 0));
+
+            return results.Max() - results.Min();
+        }
+    }
+}
---
+++ tests/Advent.Tests/DailyTests/Day.14.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Xunit;
+using Assert = Xunit.Assert;
+
+using Advent.Solutions.Utility;
+using Advent.Solutions.Days;
+
+namespace Advent.Tests.DailyTests
+{
+    public class Day14 : Interfaces.IDailyTest
+    {
+        public int Number => 14;
+
+        public string Input => TestHelper.GetInputFile(this);
+        public string Test => TestHelper.GetTestFile(this);
+
+        [Fact]
+        public void Test_KnownInputs()
+        {
+            var input = Test.Parse();
+            var poly = new Polymerization(input);
+            var res = poly.Solve();
+
+            long expectedResult = 1588;
+            Assert.Equal(expectedResult, res);
+        }
+
+        [Fact]
+        public void PartOne()
+        {
+            var poly = new Polymerization(Input.Parse());
+            var res = poly.Solve();
+
+            long expectedResult = 3048;
+            Assert.Equal(expectedResult, res);
+        }
+
+        [Fact]
+        public void PartTwo()
+        {
+            var poly = new Polymerization(Input.Parse());
+            var res = poly.Solve(40);
+
+            long expectedResult = 3288891573057;
+            Assert.Equal(expectedResult, res);
+        }
+    }
+}
